4355ca399843a5db00fb7a19883bd830d9b50822
MaqsoodSheik-Glitch <Maqsood.Sheik10@gmail.com>
2026-03-01 18:43:17 -0600

    Update prompts for user input in sub.sh
    Changed Number to no

--- scripts/sub.sh
+++ scripts/sub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -n "Enter first number: "
+echo -n "Enter first no: "
 read -r n1
 
-echo -n "Enter second number: "
+echo -n "Enter second no: "
 read -r n2
 
 echo "Sum = $((n1 + n2))"
